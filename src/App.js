11ae6bc419ac5e10f876acf7dedd1ecd72ae86d6
jawale204 <jawale204@gmail.com>
2022-04-02 11:52:46 +0530

    conditonal jsx 2-11

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ function App() {
   const body =
     "this is my first blog post and i am really glad to start this journey";
   const title = "By Blog";
+  const showComments = true;
   return (
     <div className="container">
       <h1>{title}</h1>
       <h3>{body}</h3>
-      <div className="Comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
+      {showComments ? (
+        <div className="Comments">
+          <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map((comment, index) => (
+              <li key={index}>{comment.text}</li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
     </div>
   );
 }
