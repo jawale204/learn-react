ec756d223b0586ddbcea451d47a7aef914badcd8
jawale204 <jawale204@gmail.com>
2022-04-02 12:20:34 +0530

    conditonal jsx 2-11-1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 function App() {
-  const comments = [
-    { id: 1, text: "nice videos" },
-    { id: 2, text: "keep it up" },
-    { id: 3, text: "kinda fucked up" },
-  ];
-  const body =
-    "this is my first blog post and i am really glad to start this journey";
-  const title = "By Blog";
-  const showComments = true;
   return (
     <div className="container">
-      <h1>{title}</h1>
-      <h3>{body}</h3>
-      {showComments ? (
-        <div className="Comments">
-          <h3>Comments ({comments.length})</h3>
-          <ul>
-            {comments.map((comment, index) => (
-              <li key={index}>{comment.text}</li>
-            ))}
-          </ul>
-        </div>
-      ) : null}
+      <h1>My blog</h1>
     </div>
   );
 }
