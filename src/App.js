89188d2b3f25ad3e9b4d0b1bc5f4b3f6ec632786
jawale204 <jawale204@gmail.com>
2022-04-03 14:42:28 +0530

    styling component 3-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
 
 function App() {
+  const style = {
+    backgroundColor: "white",
+    color: "black",
+  };
   return (
     <>
-      <Header text="feedback UI" />
+      <Header text="feedback UI" style={style} />
       <div className="container">
         <h1>My blog</h1>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
 import PropTypes from "prop-types";
 
 function Header(props) {
-  return <h1>{props.text}</h1>;
+  return (
+    <header style={props.style}>
+      <div className="container">
+        <h1>{props.text}</h1>
+      </div>
+    </header>
+  );
 }
 
 Header.defaultProps = {
   text: "Hello World",
+  style: {
+    backgroundColor: "rgba(0,0,0,0.4)",
+    color: "#ff6a95",
+  },
 };
 Header.propTypes = {
   text: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  color: PropTypes.string,
 };
 export default Header;
