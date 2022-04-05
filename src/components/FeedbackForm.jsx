94e14306fc56aed8560d17b82941d7607e2b446c
jawale204 <jawale204@gmail.com>
2022-04-05 14:52:22 +0530

    real-time validation 4-21(logic problem)

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,8 +3,22 @@ import Card from "./shared/Card";
 import Button from "./shared/Button";
 function FeedbackForm() {
   const [review, setReview] = useState("");
+  const [message, setMessage] = useState(false);
+  const [disabled, setDisabled] = useState(true);
   const handleTextChange = (e) => {
-    setReview(e.target.value);
+    console.log(e.target.value, e.target.value.length);
+    if (e.target.value === "") {
+      setMessage(false);
+      setDisabled(false);
+    }
+    if (e.target.value !== "" && e.target.value.trim().length < 10) {
+      setMessage(true);
+      setDisabled(true);
+    } else {
+      setMessage(false);
+      setDisabled(false);
+    }
+    setReview(e.target.value.trim());
   };
   return (
     <Card>
@@ -15,10 +29,13 @@ function FeedbackForm() {
           placeholder="write a review"
           onChange={handleTextChange}
         />
-        <Button type="submit" isDisabled={false} version="primary">
+        <Button type="submit" isDisabled={disabled} version="primary">
           Submit
         </Button>
       </div>
+      {message && (
+        <div className="message">Text must be atleast 10 in length</div>
+      )}
     </Card>
   );
 }
